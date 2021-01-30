657d6ac197df9439031f93997ae10706289258ac
portfolio <anushthaniranjan@gmail.com>
2021-01-30 13:05:36 +0530

    Check no of times u clicked a button using socketio

---
+++ public/js/chat.js
@@ -0,0 +1,10 @@
+const socket = io()
+
+socket.on('countUpdated', (count) => {
+console.log('Count has been updated', count)
+})
+
+document.querySelector('#increment').addEventListener('click', () => {
+    console.log('Clicked')
+    socket.emit('increment')
+})
